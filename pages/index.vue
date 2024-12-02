10b8e368d4bd2b70c4c3175c3a0f07a3a54e37e6
root <root@LAPTOP-50JRBB77.>
2024-12-02 22:57:07 +0300

    experiments

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+  <ClientOnly>
     <p>Test</p>
     <div class="flex items-center justify-center">
       <GlareCard class="flex flex-col items-center justify-center">
@@ -6,4 +7,17 @@
         <p className="text-black font-bold text-xl mt-4">Inspira UI</p>
       </GlareCard>
     </div>
-  </template>
+  </ClientOnly>
+  <ClientOnly>
+    <BlurReveal
+      :delay="0.2"
+      :duration="0.75"
+      class="p-8"
+    >
+      <h2 class="text-3xl font-bold tracking-tighter xl:text-6xl/none sm:text-5xl">Hey there 👋</h2>
+      <span class="text-pretty text-xl tracking-tighter xl:text-4xl/none sm:text-3xl">
+        How is it going?
+      </span>
+    </BlurReveal>
+  </ClientOnly>
+</template>
