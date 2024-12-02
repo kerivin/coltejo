9c45b9dfd0e2e4751126bec3d0f935e83a7d1d37
root <root@LAPTOP-50JRBB77.>
2024-12-02 21:06:29 +0300

    experiments

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,8 @@
 <template>
-    <div class="p-4">
-      <div class="max-w-sm rounded overflow-hidden shadow-lg mb-4">
-        <img
-          class="w-full"
-          src="https://tailwindcss.com/img/card-top.jpg"
-          alt="Sunset in the mountains"
-        />
-        <div class="px-6 py-4 pb-2">
-          <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-          <p class="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-            quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-            nihil.
-          </p>
-        </div>
-        <div class="px-6 py-4">
-          <span class="badge mr-2">#photography</span>
-          <span class="badge mr-2">#travel</span>
-          <span class="badge">#winter</span>
-        </div>
-      </div>
+    <div class="flex items-center justify-center">
+      <GlareCard class="flex flex-col items-center justify-center">
+        <NuxtImg src="https://tailwindcss.com/img/card-top.jpg" />
+        <p className="text-white font-bold text-xl mt-4">Inspira UI</p>
+      </GlareCard>
     </div>
   </template>
-  
-  <style lang="postcss" scoped>
-  .badge {
-    @apply inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700;
-    &:hover {
-      @apply bg-gray-300;
-    }
-  }
-  </style>
-  
