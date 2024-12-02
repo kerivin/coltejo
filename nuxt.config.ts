5e9e91cc88c5618aa2c6123ea09560529d4c4d54
root <root@LAPTOP-50JRBB77.>
2024-12-02 23:00:45 +0300

    ssr: true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+
+  ssr: true,
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss',
