8035ce42c78bae2e59d671c837f6e567d913a492
root <root@LAPTOP-50JRBB77.>
2024-12-02 23:01:39 +0300

    .

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,11 @@
 <template>
-  <ClientOnly>
-    <p>Test</p>
-    <div class="flex items-center justify-center">
-      <GlareCard class="flex flex-col items-center justify-center">
-        <NuxtImg src="https://tailwindcss.com/img/card-top.jpg" />
-        <p className="text-black font-bold text-xl mt-4">Inspira UI</p>
-      </GlareCard>
-    </div>
-  </ClientOnly>
+  <p>Test</p>
+  <div class="flex items-center justify-center">
+    <GlareCard class="flex flex-col items-center justify-center">
+      <NuxtImg src="https://tailwindcss.com/img/card-top.jpg" />
+      <p className="text-black font-bold text-xl mt-4">Inspira UI</p>
+    </GlareCard>
+  </div>
   <ClientOnly>
     <BlurReveal
       :delay="0.2"
