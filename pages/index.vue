1e697cb14af9e4b76718216ccff8f380efdb7ccd
root <root@LAPTOP-50JRBB77.>
2024-12-02 22:38:58 +0300

    cssPath fix

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="flex items-center justify-center">
       <GlareCard class="flex flex-col items-center justify-center">
         <NuxtImg src="https://tailwindcss.com/img/card-top.jpg" />
-        <p className="text-white font-bold text-xl mt-4">Inspira UI</p>
+        <p className="text-black font-bold text-xl mt-4">Inspira UI</p>
       </GlareCard>
     </div>
   </template>
