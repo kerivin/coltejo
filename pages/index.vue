9a16d9ce11e93d38c9aabcb2d08074211fb337ef
root <root@LAPTOP-50JRBB77.>
2024-12-03 03:23:51 +0300

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,8 @@
 <template>
-  <p>Test</p>
   <div class="flex items-center justify-center">
     <GlareCard class="flex flex-col items-center justify-center">
       <NuxtImg src="https://tailwindcss.com/img/card-top.jpg" />
-      <p className="text-black font-bold text-xl mt-4">Card</p>
+      <p className="text-white font-bold text-xl mt-4">Card</p>
     </GlareCard>
   </div>
-  <ClientOnly>
-    <BlurReveal
-      :delay="0.2"
-      :duration="0.75"
-      class="p-8"
-    >
-      <h2 class="text-3xl font-bold tracking-tighter xl:text-6xl/none sm:text-5xl">Hey there 👋</h2>
-      <span class="text-pretty text-xl tracking-tighter xl:text-4xl/none sm:text-3xl">
-        How is it going?
-      </span>
-    </BlurReveal>
-  </ClientOnly>
 </template>
