480f971909750dc5c5756a0080d2f2732efe14ba
kerivin <root@LAPTOP-50JRBB77.>
2024-12-08 01:45:44 +0300

    minor improvememnts

--- components/content/Cards.vue
+++ components/content/Cards.vue
@@ -6,40 +6,40 @@ const props = defineProps<CardsProps>();
 
 // https://www.reddit.com/r/tailwindcss/comments/10vygle/tailwind_vue_dynamic_classes/
 const grid = computed<string>(() => {
-  var str = "grid gap-4 grid-cols-1";
+  var str = "grid gap-4 grid-cols-1 + md:";
   switch(props.columns) {
     case 2:
-      str += " lg:grid-cols-2";
+      str += "grid-cols-2";
       break;
     case 3:
-      str += " lg:grid-cols-3";
+      str += "grid-cols-3";
       break;
     case 4:
-      str += " lg:grid-cols-4";
+      str += "grid-cols-4";
       break;
     case 5:
-      str += " lg:grid-cols-5";
+      str += "grid-cols-5";
       break;
     case 6:
-      str += " lg:grid-cols-6";
+      str += "grid-cols-6";
       break;
     case 7:
-      str += " lg:grid-cols-7";
+      str += "grid-cols-7";
       break;
     case 8:
-      str += " lg:grid-cols-8";
+      str += "grid-cols-8";
       break;
     case 9:
-      str += " lg:grid-cols-9";
+      str += "grid-cols-9";
       break;
     case 10:
-      str += " lg:grid-cols-10";
+      str += "grid-cols-10";
       break;
     case 11:
-      str += " lg:grid-cols-11";
+      str += "grid-cols-11";
       break;
     case 12:
-      str += " lg:grid-cols-12";
+      str += "grid-cols-12";
       break;
   }
   console.log(str);
