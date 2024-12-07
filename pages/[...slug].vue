ecbbe14f1e6523dc15712febfd9410497e2cc053
kerivin <root@LAPTOP-50JRBB77.>
2024-12-08 00:52:58 +0300

    swap h1 and content z-index

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,8 +1,8 @@
 <template>
   <main>
     <div class="flex justify-center items-center h-screen bg-green">
-      <ContentDoc />
       <h1 class="text-4xl absolute top-5">Coltejo</h1>
+      <ContentDoc />
     </div>
   </main>
 </template>
